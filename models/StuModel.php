9adc86ff3f70cfdea05848a603bfc10b21493611
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-16 18:17:16 +0530

    Add files via upload

--- models/StuModel.php
+++ models/StuModel.php
@@ -12,10 +12,28 @@ class StuModel extends CI_Model
 
    }	
 
+   function UpdateStudent($rno,$sname,$branch,$fees)
+   {
+     $this->db->where('Rno',$rno);
+    $res= $this->db->update("Student",array("Sname"=>$sname,"Branch"=>$branch,"Fees"=>$fees));
+    return $res;
+   }
+    function DeleteStudent($rno)
+   {
+    
+    $res= $this->db->delete("Student",array("Rno"=>$rno));
+    return $res;
+   }
    function ViewStudent()
    {
        $res= $this->db->get("student");
        return $res->result_array();
+
+   }
+   function EditStudent($rno)
+   {
+      $res= $this->db->get_where('Student',array("Rno"=>$rno));
+      return $res->result_array();
 
    }
 
