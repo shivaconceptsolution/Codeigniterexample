a73693417696ea0ab6588b8c2429b74069101610
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-16 18:16:09 +0530

    Add files via upload

--- controllers/Student.php
+++ controllers/Student.php
@@ -29,6 +29,22 @@ class Student extends CI_Controller
      	$this->load->view('sview',$data);
      }
 
+    function EditRecord($id)
+    {
+        $res=$this->stumodel->EditStudent($id);
+          $data = array("key"=>$res);
+      $this->load->view('editview',$data);
+    }
+     function UpdateStudent()
+     {
+       $res=$this->stumodel->UpdateStudent($this->input->post('txtrno'),$this->input->post('txtname'),$this->input->post('txtbranch'),$this->input->post('txtfees'));
+       redirect('Student/viewstudent');
+     }
+    function DeleteRecord($id)
+    {
+       $res=$this->stumodel->DeleteStudent($id);
+       redirect('Student/viewstudent');
+    }
 
 }
 
