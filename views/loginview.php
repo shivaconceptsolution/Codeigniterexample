dc4b0f7136063bdd73abb77b6d3690243a6cbd77
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-23 17:37:02 +0530

    Add files via upload

---
+++ views/loginview.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="<?php echo site_url(); ?>/login/logincode" method="post">
+  <input type="text" name="txtuser" placeholder="enter username" />
+  <br>
+  <br>
+  <input type="text" name="txtpass" placeholder="enter password" />
+  <br>
+  <br>
+  <input type="submit" name="btnsubmit" value="Login"/>
+
+</form>
+</body>
+</html>
